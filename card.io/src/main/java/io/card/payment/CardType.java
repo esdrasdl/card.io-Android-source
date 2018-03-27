caf306f27eeca68cd46533c1592776535559f40d
Esdras Degaspari <esdrasdl@gmail.com>
2018-03-27 14:24:41 -0300

    Improve Elo parsing

--- card.io/src/main/java/io/card/payment/CardType.java
+++ card.io/src/main/java/io/card/payment/CardType.java
@@ -353,7 +353,7 @@ public enum CardType {
             return CardType.INSUFFICIENT_DIGITS;
         } else if (possibleCardTypes.size() == 1) {
             CardType currentType = possibleCardTypes.iterator().next();
-            if (currentType == MAESTRO || currentType == DINERSCLUB) {
+            if (currentType == MAESTRO || currentType == DINERSCLUB || currentType == DISCOVER) {
                 if (CreditCardNumber.isValidEloCard(numStr)) {
                     currentType = ELO;
                 } else if (CreditCardNumber.isValidHiperCard(numStr)) {
